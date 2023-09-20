9de08a3bf7469bfc44d9601c09843a24721c148b
Dmitriy Dultsev <tsukikohaya@gmail.com>
2023-09-20 19:45:30 +0300

    fix dto

--- src/user/dto/userQueryDto.ts
+++ src/user/dto/userQueryDto.ts
@@ -1,12 +1,15 @@
-import { IsEmail, IsMobilePhone, IsPhoneNumber, IsString, isMobilePhone } from "class-validator";
+import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
 
 export class UserQueryDto {
    @IsString()
+   @MinLength(3)
    name: string
 
    @IsEmail()
    email: string
 
    @IsString()
+   @MinLength(10)
+   @MaxLength(12)
    phone: string
 }
